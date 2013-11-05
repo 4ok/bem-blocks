56688b0753934be7ced953ad381d3179213f7933
Aleksey Polyakov <aleksey@polyakov.com>
2013-11-06 02:44:29 +0400

    Добавлена функциональность для tabs-slides

--- common.blocks/tabs/tabs.browser.js
+++ common.blocks/tabs/tabs.browser.js
@@ -5,9 +5,15 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
         live: function() {
 
             this.liveBindTo('item', 'click', function(e) {
-                var $elem = $(e.currentTarget);
+                var $elem     = $(e.currentTarget);
+                var params    = this.elemParams($elem);
+                var $prevElem = this.elem('item', 'current', true);
 
-                this.setMod($elem, 'state', 'current');
+                if ($prevElem.length) {
+                    this.delMod($prevElem, 'current');
+                }
+                this.setMod($elem, 'current', true);
+                this.emit('current', params);
             });
         }
     });
