fe4aece518f16e7966cc91a72145d26fd32dd801
Aleksey Polyakov <apolyakov@iAm.local>
2013-11-21 21:36:21 +0400

    Различные правки

--- common.blocks/tabs-slides/tabs-slides.browser.js
+++ common.blocks/tabs-slides/tabs-slides.browser.js
@@ -8,16 +8,64 @@ modules.define('i-bem__dom', function(provide, DOM) {
 
                 inited: function()
                 {
-                    var $tabsBlock   = this.findBlockInside('tabs');
-                    var $slidesBlock = this.findBlockInside('slides');
+                    var $tabsBlock = this.findBlockInside('tabs');
+                    var _this      = this;
 
                     $tabsBlock.on('current', function(e, params)
                     {
-                        $slidesBlock.showSlide(params.slideMod[0], params.slideMod[1]);
-                    });
+                        if (params.removeCurrentSlide) {
+                            _this._removeCurrentSlide();
+                        } else if (params.setCurrentSlide) {
 
+                            for (var modName in params.setCurrentSlide) {
+                                var modVal = params.setCurrentSlide[modName];
+
+                                _this._setCurrentSlide(modName, modVal);
+                                break;
+                            }
+                        }
+                    });
                 }
             }
+        },
+
+        _setCurrentSlide: function(modName, modVal)
+        {
+            var $slidesBlock = this._getSlidesBlock();
+
+            var $elem        = $slidesBlock.elem('item', modName, modVal);
+            var $currentElem = this._getSlidesBlockCurrentElem();
+
+            if ($currentElem.length) {
+                $slidesBlock.delMod($currentElem, 'current');
+            }
+
+            if ($elem.length) {
+                $slidesBlock.setMod($elem, 'current', true);
+            }
+        },
+
+        _removeCurrentSlide: function()
+        {
+            var $slidesBlock = this._getSlidesBlock();
+
+            var $currentElem = this._getSlidesBlockCurrentElem();
+
+            if ($currentElem.length) {
+                $slidesBlock.delMod($currentElem, 'current');
+            }
+        },
+
+        _getSlidesBlockCurrentElem: function()
+        {
+            var $slidesBlock = this._getSlidesBlock();
+
+            return $slidesBlock.elem('item', 'current', true);
+        },
+
+        _getSlidesBlock: function()
+        {
+            return this.findBlockInside('slides');
         }
     });
 
