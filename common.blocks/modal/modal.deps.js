1f9eff159926446f33ec64ca560beb988e37707f
lipolyakov <lipolyakov@yandex-team.ru>
2016-09-01 04:56:23 +0300

    Add modal block

---
+++ common.blocks/modal/modal.deps.js
@@ -0,0 +1,10 @@
+({
+    shouldDeps : [
+        {
+            block : 'page',
+            mods : {
+                freezed : true,
+            },
+        },
+    ],
+});
---
+++ common.blocks/modal/modal.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+
+/**
+ * @module address-map
+ */
+modules.define(
+    'modal',
+    ['i-bem__dom'],
+    function modal(provide, bemDom) {
+
+        /**
+         * @exports
+         * @class address-map
+         * @bem
+         */
+        provide(bemDom.decl(this.name, /** @lends address-map.prototype */{
+
+            onSetMod : {
+                js : {
+                    inited() {
+                        this.__base.apply(this);
+                        this._page = this.findBlockOutside('page');
+                    },
+                },
+                visible : {
+                    '*' : function visibleToggle(modName, modVal) {
+                        this._page.setMod('freezed', modVal);
+                    },
+                },
+            },
+        }));
+    }
+);
